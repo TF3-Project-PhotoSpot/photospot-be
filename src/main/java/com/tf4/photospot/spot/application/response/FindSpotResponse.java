29b5b4ba9ae0ef534420d19d5553cae0af7cb345
code-tree <dltjdqls1200@naver.com>
2023-11-15 12:53:58 +0900

    [PS-13] refactor: 스팟 조회시 주소도 같이 반환

--- src/main/java/com/tf4/photospot/spot/application/response/FindSpotResponse.java
+++ src/main/java/com/tf4/photospot/spot/application/response/FindSpotResponse.java
@@ -9,13 +9,15 @@ import com.tf4.photospot.spot.domain.Spot;
 public record FindSpotResponse(
 	Boolean isSpot,
 	Long id,
+	String address,
 	CoordinateDto coord
 ) {
 	public static FindSpotResponse toSpotResponse(Spot spot) {
-		return new FindSpotResponse(Boolean.TRUE, spot.getId(), PointConverter.convert(spot.getCoord()));
+		return new FindSpotResponse(Boolean.TRUE, spot.getId(), spot.getAddress(),
+			PointConverter.convert(spot.getCoord()));
 	}
 
-	public static FindSpotResponse toNonSpotResponse(Point coord) {
-		return new FindSpotResponse(Boolean.FALSE, null, PointConverter.convert(coord));
+	public static FindSpotResponse toNonSpotResponse(Point coord, String address) {
+		return new FindSpotResponse(Boolean.FALSE, null, address, PointConverter.convert(coord));
 	}
 }
