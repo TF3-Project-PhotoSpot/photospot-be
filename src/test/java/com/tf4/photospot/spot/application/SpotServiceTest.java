66567e1830afdcd2ee883b952dc2775f952294be
code-tree <dltjdqls1200@naver.com>
2023-11-25 13:59:49 +0900

    [PS-22] test: 주변 스팟 조회 통합 테스트 작성

--- src/test/java/com/tf4/photospot/spot/application/SpotServiceTest.java
+++ src/test/java/com/tf4/photospot/spot/application/SpotServiceTest.java
@@ -4,6 +4,7 @@ import static com.tf4.photospot.global.util.PointConverter.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
@@ -18,6 +19,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.tf4.photospot.global.exception.ApiException;
 import com.tf4.photospot.global.exception.domain.MapErrorCode;
 import com.tf4.photospot.spot.application.request.FindSpotRequest;
+import com.tf4.photospot.spot.application.request.NearbySpotRequest;
+import com.tf4.photospot.spot.application.response.NearbySpotListResponse;
 import com.tf4.photospot.spot.domain.MapApiClient;
 import com.tf4.photospot.spot.domain.Spot;
 import com.tf4.photospot.spot.domain.SpotRepository;
@@ -33,6 +36,47 @@ class SpotServiceTest {
 
 	@MockBean
 	private MapApiClient mapApiClient;
+
+	@DisplayName("반경 내에 위치한 주변 스팟 조회")
+	@Nested
+	class NearbySpotTest {
+		@DisplayName("중심에서 직선 거리 반경 이내에 있는 장소만 조회한다.")
+		@Test
+		void getNearbySpotWithinRadius() {
+			//given
+			int radius = 5000; // 반경 5km
+			Point centerCoord = convert(127.0468177, 37.6676198);
+			Spot centerSpot = new Spot("서울시 도봉구 마들로 646", centerCoord);
+			Spot innerSpot = new Spot("중심에서 도보 3.8km", convert(127.0273281, 37.6400187));
+			Spot outerSpot = new Spot("중심에서 도보 5.8km", convert(127.0467641, 37.7155734));
+			spotRepository.saveAll(List.of(centerSpot, innerSpot, outerSpot));
+
+			//when
+			NearbySpotListResponse response = spotService.getNearbySpotList(new NearbySpotRequest(centerCoord, radius));
+
+			//then
+			assertThat(response.spots()).hasSize(2);
+			assertThat(response.spots())
+				.extracting("id")
+				.containsExactlyInAnyOrder(centerSpot.getId(), innerSpot.getId());
+		}
+
+		@DisplayName("중심에서 직선 거리 이내에 장소가 없을 경우 빈 List가 반환된다")
+		@Test
+		public void getNearbySpotEmptyList() {
+			//given
+			int radius = 5000; // 반경 5km
+			Point centerCoord = convert(127.0468177, 37.6676198);
+			Spot outerSpot = new Spot("중심에서 도보 5.8km", convert(127.0467641, 37.7155734));
+			spotRepository.save(outerSpot);
+
+			//when
+			NearbySpotListResponse response = spotService.getNearbySpotList(new NearbySpotRequest(centerCoord, radius));
+
+			//then
+			assertThat(response.spots()).isEmpty();
+		}
+	}
 
 	@Nested
 	@DisplayName("특정 좌표에 포함되는 스팟 조회")
@@ -64,7 +108,7 @@ class SpotServiceTest {
 		void findRegisteredSpot() {
 			//given
 			var coord = convert(127.046817765572, 37.6676198504815);
-			var spot = new Spot("서울시 마들로 646", coord, 0L);
+			var spot = new Spot("서울시 마들로 646", coord);
 			spotRepository.save(spot);
 
 			given(mapApiClient.findAddressByCoordinate(any(Point.class)))
