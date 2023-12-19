1636f3955ee448f262b82fe719d83de099eee614
code-tree <dltjdqls1200@naver.com>
2023-12-19 17:40:44 +0900

    [PS-44] refactor: ControllerAdvice 리펙토링, validation error 반환하도록 수정

--- src/main/java/com/tf4/photospot/global/exception/ApiException.java
+++ src/main/java/com/tf4/photospot/global/exception/ApiException.java
@@ -1,22 +1,10 @@
 package com.tf4.photospot.global.exception;
 
-import org.springframework.http.HttpStatusCode;
-
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @RequiredArgsConstructor
 public class ApiException extends RuntimeException {
 	private final ApiErrorCode errorCode;
-
-	public HttpStatusCode getStatusCode() {
-		return errorCode.getStatusCode();
-	}
-
-	public String getMessage() {
-		return errorCode.getMessage();
-	}
-
-	public String getName() {
-		return errorCode.name();
-	}
 }
---
+++ src/main/java/com/tf4/photospot/global/exception/domain/CommonErrorCode.java
@@ -0,0 +1,19 @@
+package com.tf4.photospot.global.exception.domain;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+import com.tf4.photospot.global.exception.ApiErrorCode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum CommonErrorCode implements ApiErrorCode {
+	INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "Invalid Paramete"),
+	UNEXPECTED_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "unexpected error");
+
+	private final HttpStatusCode statusCode;
+	private final String message;
+}
