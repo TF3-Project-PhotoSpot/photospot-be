9af03434fb28490f185bec0d58ed487afc976a1d
Ojeegu <lumirite0@gmail.com>
2023-12-27 02:38:43 +0900

    [PS-42] feat: Jwt 검증 필터 추가

---
+++ src/main/java/com/tf4/photospot/global/filter/JwtTokenValidatorFilter.java
@@ -0,0 +1,58 @@
+package com.tf4.photospot.global.filter;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.tf4.photospot.auth.application.JwtService;
+import com.tf4.photospot.global.config.jwt.JwtConstant;
+import com.tf4.photospot.global.config.security.SecurityConstant;
+import com.tf4.photospot.global.dto.LoginUserDto;
+
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class JwtTokenValidatorFilter extends OncePerRequestFilter {
+
+	private final JwtService jwtService;
+
+	// Todo : 예외 처리
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
+
+		String jwt = request.getHeader(JwtConstant.AUTHORIZATION_HEADER);
+		if (jwt == null) {
+			throw new RuntimeException("jwt is null");
+		}
+
+		// Todo : try-catch 문이 불필요해보이는데 보수적으로 넣을지 & 예외 처리
+		try {
+			Claims claims = jwtService.parse(jwt);
+			Long userId = Long.valueOf(claims.getId());
+			String authorities = String.valueOf(claims.get(JwtConstant.USER_AUTHORITIES));
+			Authentication auth = new UsernamePasswordAuthenticationToken(new LoginUserDto(userId), null,
+				AuthorityUtils.commaSeparatedStringToAuthorityList(authorities));
+			SecurityContextHolder.getContext().setAuthentication(auth);
+		} catch (Exception ex) {
+			throw new RuntimeException("invalid jwt");
+		}
+
+		filterChain.doFilter(request, response);
+	}
+
+	// 로그인 요청 API에서는 동작하지 않음
+	@Override
+	protected boolean shouldNotFilter(HttpServletRequest request) {
+		return request.getRequestURI().equals(SecurityConstant.LOGIN_URL);
+	}
+}
