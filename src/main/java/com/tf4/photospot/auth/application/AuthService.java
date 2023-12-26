b37337a7bf18dbb0821b523c22a7a3d2e04c9221
Ojeegu <lumirite0@gmail.com>
2023-12-27 02:39:38 +0900

    [PS-42] refactor: login Request Mapping 메서드 삭제

--- src/main/java/com/tf4/photospot/auth/application/AuthService.java
+++ src/main/java/com/tf4/photospot/auth/application/AuthService.java
@@ -3,10 +3,8 @@ package com.tf4.photospot.auth.application;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.tf4.photospot.auth.application.response.LoginTokenResponse;
 import com.tf4.photospot.auth.application.response.ReissueTokenResponse;
 import com.tf4.photospot.user.application.UserService;
-import com.tf4.photospot.user.application.response.UserLoginResponse;
 import com.tf4.photospot.user.domain.User;
 
 import io.jsonwebtoken.Claims;
@@ -20,17 +18,10 @@ public class AuthService {
 	private final UserService userService;
 	private final JwtService jwtService;
 
-	@Transactional
-	public LoginTokenResponse login(String providerType, String account) {
-		UserLoginResponse loginUser = userService.oauthLogin(providerType, account);
-		return jwtService.issueTokens(loginUser.hasLoggedInBefore(),
-			userService.findUser(providerType, loginUser.getAccount()));
-	}
-
 	public ReissueTokenResponse reissueToken(String refreshToken) {
 		Claims claims = jwtService.parse(refreshToken);
 		User user = userService.findUser(claims.get("id", Long.class));
 		jwtService.validRefreshToken(user.getId(), refreshToken);
-		return new ReissueTokenResponse(jwtService.reissueAccessToken(user));
+		return new ReissueTokenResponse(jwtService.issueAccessToken(user.getId(), user.getRole()));
 	}
 }
--- src/main/java/com/tf4/photospot/auth/presentation/AuthController.java
+++ src/main/java/com/tf4/photospot/auth/presentation/AuthController.java
@@ -3,11 +3,9 @@ package com.tf4.photospot.auth.presentation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tf4.photospot.auth.application.AuthService;
-import com.tf4.photospot.auth.application.response.LoginTokenResponse;
 import com.tf4.photospot.auth.application.response.ReissueTokenResponse;
 import com.tf4.photospot.auth.presentation.request.ReissueRequest;
 import com.tf4.photospot.global.dto.ApiResponse;
@@ -20,12 +18,6 @@ import lombok.RequiredArgsConstructor;
 public class AuthController {
 
 	private final AuthService authService;
-
-	@GetMapping("/login")
-	public ApiResponse<LoginTokenResponse> login(@RequestParam("providerType") String providerType,
-		@RequestParam("account") String account) {
-		return ApiResponse.success(authService.login(providerType, account));
-	}
 
 	@GetMapping("/reissue")
 	public ApiResponse<ReissueTokenResponse> reissueToken(
