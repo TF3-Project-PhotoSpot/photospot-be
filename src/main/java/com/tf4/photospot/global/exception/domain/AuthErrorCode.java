3e015598213e44dd5cbdf675461b838b0d563076
Ojeegu <lumirite0@gmail.com>
2023-12-27 02:21:54 +0900

    [PS-42] feat: 커스텀 예외 코드 추가

--- src/main/java/com/tf4/photospot/global/exception/domain/AuthErrorCode.java
+++ src/main/java/com/tf4/photospot/global/exception/domain/AuthErrorCode.java
@@ -14,7 +14,8 @@ public enum AuthErrorCode implements ApiErrorCode {
 
 	EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
 	INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
-	UNAUTHORIZED_USER(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다.");
+	UNAUTHORIZED_USER(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
+	INVALID_PROVIDER_TYPE(HttpStatus.BAD_REQUEST, "유효하지 않은 공급자입니다.");
 
 	private final HttpStatusCode statusCode;
 	private final String message;
