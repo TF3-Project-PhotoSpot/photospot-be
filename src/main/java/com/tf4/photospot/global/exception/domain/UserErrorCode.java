ac1bb02270b990053d43b15cd6b35daf413e66e1
Ojeegu <lumirite0@gmail.com>
2023-12-29 00:41:09 +0900

    [PS-42] feat: 유효하지 않는 사용자 조회 시 예외 발생

---
+++ src/main/java/com/tf4/photospot/global/exception/domain/UserErrorCode.java
@@ -0,0 +1,19 @@
+package com.tf4.photospot.global.exception.domain;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+import com.tf4.photospot.global.exception.ApiErrorCode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserErrorCode implements ApiErrorCode {
+
+	NOT_FOUND_USER(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다.");
+
+	private final HttpStatusCode statusCode;
+	private final String message;
+}
--- src/main/java/com/tf4/photospot/user/application/UserService.java
+++ src/main/java/com/tf4/photospot/user/application/UserService.java
@@ -3,6 +3,8 @@ package com.tf4.photospot.user.application;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.tf4.photospot.global.exception.ApiException;
+import com.tf4.photospot.global.exception.domain.UserErrorCode;
 import com.tf4.photospot.user.application.request.LoginUserInfo;
 import com.tf4.photospot.user.application.response.OauthLoginResponse;
 import com.tf4.photospot.user.domain.User;
@@ -18,7 +20,6 @@ public class UserService {
 
 	private final UserRepository userRepository;
 
-	// Todo : 이름 변경
 	@Transactional
 	public OauthLoginResponse oauthLogin(String providerType, String account) {
 		return userRepository.findUserByProviderTypeAndAccount(providerType, account)
@@ -28,16 +29,9 @@ public class UserService {
 			)));
 	}
 
-	// Todo : 예외 처리
 	public User findUser(Long userId) {
 		return userRepository.findById(userId)
-			.orElseThrow();
-	}
-
-	// Todo : 예외 처리
-	public User findUser(String providerType, String account) {
-		return userRepository.findUserByProviderTypeAndAccount(providerType, account)
-			.orElseThrow();
+			.orElseThrow(() -> new ApiException(UserErrorCode.NOT_FOUND_USER));
 	}
 
 	private boolean isNicknameDuplicated(String nickname) {
