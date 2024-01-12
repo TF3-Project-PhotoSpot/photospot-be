4a087c2b95ffee868607a5d7f383f7cf1ad34875
Ojeegu <lumirite0@gmail.com>
2024-01-13 01:57:54 +0900

    [PS-65] style: 패키지 변경

--- src/main/java/com/tf4/photospot/user/domain/UserRepository.java
+++ src/main/java/com/tf4/photospot/user/domain/UserRepository.java
@@ -1,10 +1,8 @@
-package com.tf4.photospot.user.infrastructure;
+package com.tf4.photospot.user.domain;
 
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tf4.photospot.user.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
