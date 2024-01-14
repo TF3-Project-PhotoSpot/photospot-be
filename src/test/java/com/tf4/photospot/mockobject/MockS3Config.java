2d13ea67606b7e8de66d75b081a220664dfc2e5a
Ojeegu <lumirite0@gmail.com>
2024-01-15 06:56:26 +0900

    [PS-65] refactor: mock s3 분리

--- src/test/java/com/tf4/photospot/mockobject/MockS3Config.java
+++ src/test/java/com/tf4/photospot/mockobject/MockS3Config.java
@@ -1,19 +1,33 @@
 package com.tf4.photospot.mockobject;
 
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.mockito.Mockito;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
 
-@Configuration
+@TestConfiguration
 @Profile("test")
 public class MockS3Config {
+
+	private static final String DUMMY_URL = "https://example.com";
+
 	@Bean
 	@Primary
-	public AmazonS3 mockAmazonS3Client() {
-		return Mockito.mock(AmazonS3.class);
+	public AmazonS3 mockAmazonS3Client() throws MalformedURLException {
+		AmazonS3 amazonS3 = Mockito.mock(AmazonS3.class);
+		given(amazonS3.putObject(any(PutObjectRequest.class))).willReturn(new PutObjectResult());
+		given(amazonS3.getUrl(any(), any())).willReturn(new URL(DUMMY_URL));
+		return amazonS3;
 	}
 }
