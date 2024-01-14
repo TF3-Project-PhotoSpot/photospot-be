5a2d7662f744e61420f6f776bbc549cfcd13c6fe
Ojeegu <lumirite0@gmail.com>
2024-01-15 06:56:02 +0900

    [PS-65] feat: JpaAuditingConfig 빈 등록

--- src/main/java/com/tf4/photospot/global/config/JpaAuditingConfig.java
+++ src/main/java/com/tf4/photospot/global/config/JpaAuditingConfig.java
@@ -1,7 +1,9 @@
 package com.tf4.photospot.global.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Configuration
 @EnableJpaAuditing
 public class JpaAuditingConfig {
 }
