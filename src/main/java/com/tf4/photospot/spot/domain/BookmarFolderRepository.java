523879423adc149240386a46dfa4dea464af8e92
code-tree <dltjdqls1200@naver.com>
2024-01-16 17:29:46 +0900

    [PS-60] feat: 북마크 추가

---
+++ src/main/java/com/tf4/photospot/spot/domain/BookmarFolderRepository.java
@@ -0,0 +1,6 @@
+package com.tf4.photospot.spot.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookmarFolderRepository extends JpaRepository<BookmarkFolder, Long> {
+}
