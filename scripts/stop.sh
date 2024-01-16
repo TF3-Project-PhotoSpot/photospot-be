dd26e54da38cf54b62120807e5812de4adf9c662
code-tree <dltjdqls1200@naver.com>
2024-01-16 17:48:05 +0900

    [PS-67] chore: EOF 추가

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,11 +1,11 @@
-  CONTAINER_NAME=photospot
-  CONTAINER_ID=$(docker ps -a -q --filter "name=$CONTAINER_NAME")
-  CONTAINER_IMAGE=$(docker inspect --format='{{.Config.Image}}' $CONTAINER_ID)
+CONTAINER_NAME=photospot
+CONTAINER_ID=$(docker ps -a -q --filter "name=$CONTAINER_NAME")
+CONTAINER_IMAGE=$(docker inspect --format='{{.Config.Image}}' $CONTAINER_ID)
 
-  cd /home/ubuntu/app
+cd /home/ubuntu/app
 
-  docker-compose down
+docker-compose down
 
-  if [ -n "$CONTAINER_ID" ]; then
-    docker rmi $CONTAINER_IMAGE
-  fi
+if [ -n "$CONTAINER_ID" ]; then
+  docker rmi $CONTAINER_IMAGE
+fi
