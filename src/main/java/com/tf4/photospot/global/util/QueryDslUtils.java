098a8234481f548d82daaeb5a0c95f773773cd2a
code-tree <dltjdqls1200@naver.com>
2024-01-23 16:40:50 +0900

    [PS-68] feat: queryDsl 동적 sort util 클래스 추가

---
+++ src/main/java/com/tf4/photospot/global/util/QueryDslUtils.java
@@ -0,0 +1,37 @@
+package com.tf4.photospot.global.util;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.EntityPathBase;
+import com.querydsl.core.types.dsl.PathBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.tf4.photospot.post.domain.QPost;
+
+public abstract class QueryDslUtils {
+	public static <T> JPAQuery<T> orderBy(
+		JPAQuery<T> query,
+		QPost qEntity,
+		Pageable pageable
+	) {
+		for (Sort.Order order : pageable.getSort()) {
+			query.orderBy(toOrderSpecifier(qEntity, order));
+		}
+		return query;
+	}
+
+	@SuppressWarnings({"rawtypes, unchecked"})
+	private static <T> OrderSpecifier toOrderSpecifier(EntityPathBase<T> qEntity, Sort.Order order) {
+		return new OrderSpecifier(getDeriction(order),
+			new PathBuilder<>(qEntity.getType(), qEntity.getMetadata()).get(order.getProperty()));
+	}
+
+	private static Order getDeriction(Sort.Order order) {
+		if (order.isAscending()) {
+			return Order.ASC;
+		}
+		return Order.DESC;
+	}
+}
