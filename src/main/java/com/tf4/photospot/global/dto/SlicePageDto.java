54b71c28c476a680b3887a66bdfb6ac1a3cc845f
code-tree <dltjdqls1200@naver.com>
2024-01-23 16:39:44 +0900

    [PS-68] refactor: Slice convert를 전역으로 사용해서 PageUtils로 변경

---
+++ src/main/java/com/tf4/photospot/global/dto/SlicePageDto.java
@@ -0,0 +1,14 @@
+package com.tf4.photospot.global.dto;
+
+import java.util.List;
+
+import org.springframework.data.domain.Slice;
+
+public record SlicePageDto<T>(
+	List<T> content,
+	Boolean hasNext
+) {
+	public static <T> SlicePageDto<T> wrap(Slice<T> slice) {
+		return new SlicePageDto<>(slice.getContent(), slice.hasNext());
+	}
+}
