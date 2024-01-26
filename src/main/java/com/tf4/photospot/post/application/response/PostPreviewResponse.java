011d4b8256de0e7da349771c799aac3dbb4d5dde
code-tree <dltjdqls1200@naver.com>
2024-01-26 20:32:28 +0900

    [PS-71] feat: 방명록 미리보기 목록 조회 기능
    - 기존에 최신 방명록을 조회하던 것을 합쳐서 수정

--- src/main/java/com/tf4/photospot/post/application/response/PostPreviewResponse.java
+++ src/main/java/com/tf4/photospot/post/application/response/PostPreviewResponse.java
@@ -1,8 +1,10 @@
 package com.tf4.photospot.post.application.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.querydsl.core.annotations.QueryProjection;
 
 public record PostPreviewResponse(
+	@JsonIgnore
 	Long spotId,
 	Long postId,
 	String photoUrl
