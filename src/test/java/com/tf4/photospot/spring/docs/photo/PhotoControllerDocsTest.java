6d492eedf02e97145de7f068f587d2cbe6ef98ac
Ojeegu <lumirite0@gmail.com>
2024-02-05 02:24:35 +0900

    [PS-77] docs: 방명록 RestDocs 작성

--- src/test/java/com/tf4/photospot/spring/docs/photo/PhotoControllerDocsTest.java
+++ src/test/java/com/tf4/photospot/spring/docs/photo/PhotoControllerDocsTest.java
@@ -3,7 +3,6 @@ package com.tf4.photospot.spring.docs.photo;
 import static org.mockito.BDDMockito.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.DisplayName;
@@ -15,15 +14,10 @@ import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.tf4.photospot.global.dto.CoordinateDto;
 import com.tf4.photospot.photo.application.PhotoService;
 import com.tf4.photospot.photo.application.S3Uploader;
-import com.tf4.photospot.photo.application.request.PhotoSaveRequest;
-import com.tf4.photospot.photo.application.response.PhotoSaveResponse;
 import com.tf4.photospot.photo.application.response.PhotoUploadResponse;
 import com.tf4.photospot.photo.presentation.PhotoController;
-import com.tf4.photospot.photo.presentation.request.PhotoSaveHttpRequest;
 import com.tf4.photospot.spring.docs.RestDocsSupport;
 
 public class PhotoControllerDocsTest extends RestDocsSupport {
@@ -59,39 +53,6 @@ public class PhotoControllerDocsTest extends RestDocsSupport {
 				),
 				responseFields(
 					fieldWithPath("photoUrl").type(JsonFieldType.STRING).description("S3 Temp 폴더에 업로드한 사진 URL")
-				)
-			));
-	}
-
-	@Test
-	@DisplayName("사진 DB 저장")
-	void savePhoto() throws Exception {
-		// given
-		var prePhotoUrl = "https://example.com/temp/image.webp";
-		var coord = new CoordinateDto(45.0, 88.0);
-		var request = new PhotoSaveHttpRequest(prePhotoUrl, coord, "2024-01-13T05:20:18.981+09:00");
-		var postPhotoUrl = "https://example.com/post_images/image.webp";
-		var response = new PhotoSaveResponse(1L);
-
-		given(s3Uploader.moveFolder(anyString(), anyString())).willReturn(postPhotoUrl);
-		given(photoService.save(any(PhotoSaveRequest.class))).willReturn(response);
-
-		// when & then
-		mockMvc.perform(post("/api/v1/photos")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(new ObjectMapper().writeValueAsString(request))
-				.accept(MediaType.APPLICATION_JSON))
-			.andExpect(status().isOk())
-			.andDo(restDocsTemplate(
-				requestFields(
-					fieldWithPath("photoUrl").description("직전에 업로드한 사진 URL"),
-					fieldWithPath("coord").description("경도와 위도에 대한 DTO"),
-					fieldWithPath("coord.lon").description("경도"),
-					fieldWithPath("coord.lat").description("위도"),
-					fieldWithPath("takenAt").description("사진이 찍힌 시각")
-				),
-				responseFields(
-					fieldWithPath("photoId").type(JsonFieldType.NUMBER).description("저장 성공한 사진 아이디")
 				)
 			));
 	}
