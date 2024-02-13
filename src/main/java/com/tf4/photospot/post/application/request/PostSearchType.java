2dd1f7fcb8a2bac35f937811f7a977de0f654ef8
code-tree <dltjdqls1200@naver.com>
2024-02-13 15:31:27 +0900

    [PS-80] refactor: 방명록 페이징 조회 검증 로직 수정
    - 정렬 가능한 property를 PostSearchType별로 체크

--- src/main/java/com/tf4/photospot/post/application/request/PostSearchType.java
+++ src/main/java/com/tf4/photospot/post/application/request/PostSearchType.java
@@ -1,6 +1,25 @@
 package com.tf4.photospot.post.application.request;
 
+import java.util.Set;
+
+import org.springframework.data.domain.Sort;
+
+import com.tf4.photospot.global.exception.ApiException;
+import com.tf4.photospot.global.exception.domain.CommonErrorCode;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum PostSearchType {
-	MY_POSTS,
-	POSTS_OF_SPOT
+	MY_POSTS(Set.of("id")),
+	POSTS_OF_SPOT(Set.of("id", "likeCount"));
+
+	private final Set<String> sortableProperties;
+
+	public void verifyOrders(Sort sort) {
+		final boolean cannotSort = sort.stream().anyMatch(order -> !sortableProperties.contains(order.getProperty()));
+		if (cannotSort) {
+			throw new ApiException(CommonErrorCode.CANNOT_SORTED_PROPERTY);
+		}
+	}
 }
