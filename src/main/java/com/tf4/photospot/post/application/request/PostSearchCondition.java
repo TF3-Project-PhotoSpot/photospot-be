ea888f174e4591b42c9d1139e00f763f2c50e1b3
code-tree <dltjdqls1200@naver.com>
2024-02-13 15:31:27 +0900

    [PS-80] refactor: 방명록 페이징 조회 검증 로직 수정
    - 정렬 가능한 property를 PostSearchType별로 체크

--- src/main/java/com/tf4/photospot/post/application/request/PostSearchCondition.java
+++ src/main/java/com/tf4/photospot/post/application/request/PostSearchCondition.java
@@ -1,12 +1,9 @@
 package com.tf4.photospot.post.application.request;
 
-import java.util.Set;
-
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 
 import com.tf4.photospot.global.exception.ApiException;
-import com.tf4.photospot.global.exception.domain.CommonErrorCode;
+import com.tf4.photospot.global.exception.domain.PostErrorCode;
 
 import lombok.Builder;
 
@@ -16,16 +13,11 @@ public record PostSearchCondition(
 	PostSearchType type,
 	Pageable pageable
 ) {
-	private static final Set<String> SORTABLE_PROPERTIES = Set.of("id", "likeCount");
-
 	@Builder
 	public PostSearchCondition {
-		if (containsInvalidOrder(pageable.getSort())) {
-			throw new ApiException(CommonErrorCode.CANNOT_SORTED_PROPERTY);
+		if (type == null) {
+			throw new ApiException(PostErrorCode.REQUIRE_POST_SEARCH_TYPE);
 		}
-	}
-
-	private boolean containsInvalidOrder(Sort sort) {
-		return sort.stream().anyMatch(order -> !SORTABLE_PROPERTIES.contains(order.getProperty()));
+		type.verifyOrders(pageable.getSort());
 	}
 }
