4ddea3a00bb3be1c69ad962fab5a73c7693028a5
Ojeegu <lumirite0@gmail.com>
2024-02-26 10:49:34 +0900

    [PS-95] styleL: 변수명 변경

--- src/main/java/com/tf4/photospot/auth/infrastructure/JwtRedisRepository.java
+++ src/main/java/com/tf4/photospot/auth/infrastructure/JwtRedisRepository.java
@@ -15,31 +15,31 @@ import lombok.RequiredArgsConstructor;
 @Repository
 @RequiredArgsConstructor
 public class JwtRedisRepository {
-	private final RedisTemplate<String, String> redisTemplate;
+	private static final String REFRESH_TOKEN_PREFIX = "user:";
+	private static final String REVOKED_ACCESS_TOKEN_VALUE = "revoked_access_token";
 
-	private static final String PREFIX = "user:";
-	private static final String REVOKED_ACCESS_TOKEN = "revoked_access_token";
+	private final RedisTemplate<String, String> redisTemplate;
 
 	public void save(RefreshToken refreshToken) {
 		ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
-		String key = PREFIX + refreshToken.getUserId();
+		String key = REFRESH_TOKEN_PREFIX + refreshToken.getUserId();
 		valueOperations.set(key, refreshToken.getToken());
 		redisTemplate.expire(key, JwtConstant.REFRESH_TOKEN_DURATION.toMillis(), TimeUnit.MILLISECONDS);
 	}
 
 	public Optional<RefreshToken> findByUserId(Long userId) {
 		ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
-		String refreshToken = valueOperations.get(PREFIX + userId);
+		String refreshToken = valueOperations.get(REFRESH_TOKEN_PREFIX + userId);
 		return Optional.ofNullable(refreshToken).map(token -> new RefreshToken(userId, token));
 	}
 
 	public void deleteByUserId(Long userId) {
-		redisTemplate.delete(PREFIX + userId);
+		redisTemplate.delete(REFRESH_TOKEN_PREFIX + userId);
 	}
 
 	public void saveAccessTokenInBlackList(String accessToken, Long expiration) {
 		ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
-		valueOperations.set(accessToken, REVOKED_ACCESS_TOKEN);
+		valueOperations.set(accessToken, REVOKED_ACCESS_TOKEN_VALUE);
 		redisTemplate.expire(accessToken, expiration, TimeUnit.MILLISECONDS);
 	}
 
