6e2e9b62393728dbd97a741d10ab6bf0a76fc78d
Ojeegu <lumirite0@gmail.com>
2024-03-03 22:04:11 +0900

    [PS-97] style: 변수명 변경

--- src/main/java/com/tf4/photospot/auth/application/response/KakaoUnlinkResponse.java
+++ src/main/java/com/tf4/photospot/auth/application/response/KakaoUnlinkResponse.java
@@ -1,10 +1,13 @@
 package com.tf4.photospot.auth.application.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class KakaoUnlinkResponse {
-	private Long id;
+	@JsonProperty("id")
+	private Long account;
 }
