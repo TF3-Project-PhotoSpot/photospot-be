d206a061e12dcac49768ff2b2b15ca131a3a336c
Ojeegu <lumirite0@gmail.com>
2024-03-03 22:00:40 +0900

    [PS-97] docs: 회원 탈퇴 api restDocs 작성

--- src/test/java/com/tf4/photospot/spring/docs/auth/AuthControllerDocsTest.java
+++ src/test/java/com/tf4/photospot/spring/docs/auth/AuthControllerDocsTest.java
@@ -15,18 +15,20 @@ import com.tf4.photospot.auth.application.AuthService;
 import com.tf4.photospot.auth.application.response.ReissueTokenResponse;
 import com.tf4.photospot.auth.presentation.AuthController;
 import com.tf4.photospot.spring.docs.RestDocsSupport;
+import com.tf4.photospot.user.application.UserService;
 
 public class AuthControllerDocsTest extends RestDocsSupport {
 
 	private final AuthService authService = mock(AuthService.class);
+	private final UserService userService = mock(UserService.class);
 
 	@Override
 	protected Object initController() {
-		return new AuthController(authService);
+		return new AuthController(authService, userService);
 	}
 
-	@DisplayName("액세스 토큰 재발급")
 	@Test
+	@DisplayName("액세스 토큰 재발급")
 	void reissueToken() throws Exception {
 		// given
 		var reissueResponse = new ReissueTokenResponse("new_access_token_value");
@@ -44,5 +46,27 @@ public class AuthControllerDocsTest extends RestDocsSupport {
 				responseFields(
 					fieldWithPath("accessToken").type(JsonFieldType.STRING).description("재발급 된 액세스 토큰")
 				)));
+	}
+
+	@Test
+	@DisplayName("회원 탈퇴")
+	void unlinkUser() throws Exception {
+		// when
+		mockMvc.perform(post("/api/v1/auth/unlink"))
+			.andExpect(status().isOk())
+			.andDo(restDocsTemplate(
+				responseFields(fieldWithPath("message").type(JsonFieldType.STRING).description("성공"))
+			));
+	}
+
+	@Test
+	@DisplayName("회원 삭제")
+	void deleteUser() throws Exception {
+		// when
+		mockMvc.perform(post("/api/v1/auth/delete"))
+			.andExpect(status().isOk())
+			.andDo(restDocsTemplate(
+				responseFields(fieldWithPath("message").type(JsonFieldType.STRING).description("성공"))
+			));
 	}
 }
