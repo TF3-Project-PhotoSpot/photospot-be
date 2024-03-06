183bc0eb3bc58facff1ae738c69077ab666f1010
code-tree <dltjdqls1200@naver.com>
2024-03-06 16:02:22 +0900

    [PS-87] refactor: 북마크 폴더 이름 중복 체크 제거

--- src/main/java/com/tf4/photospot/bookmark/infrastructure/BookmarkQueryRepository.java
+++ src/main/java/com/tf4/photospot/bookmark/infrastructure/BookmarkQueryRepository.java
@@ -1,7 +1,5 @@
 package com.tf4.photospot.bookmark.infrastructure;
 
-import static com.tf4.photospot.bookmark.domain.QBookmarkFolder.*;
-
 import org.springframework.stereotype.Repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -13,12 +11,4 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class BookmarkQueryRepository extends QueryDslUtils {
 	private final JPAQueryFactory queryFactory;
-
-	public boolean existsFolderName(Long userId, String folderName) {
-		final Integer exists = queryFactory.selectOne()
-			.from(bookmarkFolder)
-			.where(bookmarkFolder.user.id.eq(userId).and(bookmarkFolder.name.eq(folderName)))
-			.fetchFirst();
-		return exists != null;
-	}
 }
