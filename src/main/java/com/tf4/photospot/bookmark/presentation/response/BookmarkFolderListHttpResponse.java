5205d4436b539304e4c8e6940a18bcea7e996b72
code-tree <dltjdqls1200@naver.com>
2024-03-07 18:27:41 +0900

    [PS-100] refactor: 리뷰 반영
    - 북마크 폴더 리스트 정렬 방향 추가
    - maxCount 공통값으로 수정

--- src/main/java/com/tf4/photospot/bookmark/presentation/response/BookmarkFolderListHttpResponse.java
+++ src/main/java/com/tf4/photospot/bookmark/presentation/response/BookmarkFolderListHttpResponse.java
@@ -4,7 +4,13 @@ import java.util.List;
 
 import com.tf4.photospot.bookmark.application.response.BookmarkFolderResponse;
 
+import lombok.Builder;
+
 public record BookmarkFolderListHttpResponse(
-	List<BookmarkFolderResponse> bookmarkFolders
+	List<BookmarkFolderResponse> bookmarkFolders,
+	int maxBookmarkCount
 ) {
+	@Builder
+	public BookmarkFolderListHttpResponse {
+	}
 }
